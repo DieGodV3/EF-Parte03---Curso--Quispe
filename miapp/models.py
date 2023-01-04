41e1e831f632b670a28a99aa706d2df37a08e59d
Prado Zuta Miguel Angel <mikk_pr@hotmail.com>
2023-01-04 12:06:43 -0500

    EF Parte03 - Curso - Quispe

--- miapp/models.py
+++ miapp/models.py
@@ -10,3 +10,13 @@ class Docente(models.Model):
     fecha_nacimiento=models.DateTimeField('Fecha de nacimiento')
     fecha_registro=models.DateTimeField(datetime.datetime.now())
     estado=models.CharField(max_length=10)
+
+
+class Curso(models.Model):
+    codigo=models.CharField(max_length=6,primary_key=True)
+    nombre=models.CharField(max_length=20)
+    horas=models.CharField(max_length=5)
+    credito=models.CharField(max_length=20)
+    Fecha_registro=models.DateTimeField('Fecha de nacimiento')
+    estado=models.CharField(max_length=2)
+
