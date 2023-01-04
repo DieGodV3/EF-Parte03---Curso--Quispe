6686807ed1a84efd878e39958743aad201a488fa
Sebastian Alberto <sebastianalberto258@hotmail.com>
2023-01-04 11:56:20 -0500

    EF Parte02 - Docente - Rakauskas

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,12 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+class Docente(models.Model):
+    codigo=models.CharField(max_length=6,primary_key=True)
+    nombre=models.CharField(max_length=20)
+    apellido_paterno=models.CharField(max_length=20)
+    apellido_materno=models.CharField(max_length=20)
+    dni=models.CharField(max_length=8)
+    fecha_nacimiento=models.DateTimeField('Fecha de nacimiento')
+    fecha_registro=models.DateTimeField(datetime.datetime.now())
+    estado=models.CharField(max_length=10)
